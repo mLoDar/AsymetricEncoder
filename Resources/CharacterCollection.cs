9a5c64d8752a8194ad6ea983e5972386c6890bbe
mLoDar <mlodarcoding@gmail.com>
2024-03-22 22:01:21 +0100

    Updated CharacterCollection
    Added missing special characters

--- Resources/CharacterCollection.cs
+++ Resources/CharacterCollection.cs
@@ -3,7 +3,7 @@
     internal readonly static string letters_Lowercase = "abcdefghijklmnopqrstuvwxyz";
     internal readonly static string letters_Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     internal readonly static string chars_Numbers = "0123456789";
-    internal readonly static string latin_Specials = @"^°!²§³$%&/{([)]=}ß?\´`+*~#'-_.:,;<>|@" + Convert.ToString('"');
+    internal readonly static string latin_Specials = @"^°!²§³$%&/{([)]=}ß?\´`+*~#'-_.:,;<>|@öÖäÄüÜµ€" + Convert.ToString('"');
 
     internal readonly static string allCombined = $"{letters_Lowercase}{letters_Uppercase}{chars_Numbers}{latin_Specials}";
 }
